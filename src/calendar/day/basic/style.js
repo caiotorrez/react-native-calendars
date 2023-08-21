1e587c0b150552d333259c0c9c55103ea7cfa52c
Caio Torrez <caiotorrez@live.com>
2023-08-21 12:50:42 -0300

    changed

---
+++ src/calendar/day/basic/style.js
@@ -0,0 +1,77 @@
+import { StyleSheet } from 'react-native';
+import * as defaultStyle from '../../../style';
+import constants from '../../../commons/constants';
+export default function styleConstructor(theme = {}) {
+    const appStyle = { ...defaultStyle, ...theme };
+    return StyleSheet.create({
+        container: {
+            alignSelf: 'stretch',
+            alignItems: 'center'
+        },
+        base: {
+            width: 50,
+            height: 50,
+            alignItems: 'center',
+            paddingTop: 4,
+        },
+        text: {
+            marginTop: constants.isAndroid ? 4 : 9,
+            fontSize: appStyle.textDayFontSize,
+            fontFamily: appStyle.textDayFontFamily,
+            fontWeight: appStyle.textDayFontWeight,
+            color: appStyle.dayTextColor,
+            backgroundColor: 'rgba(255, 255, 255, 0)',
+            ...appStyle.textDayStyle
+        },
+        alignedText: {
+            marginTop: constants.isAndroid ? 4 : 6
+        },
+        selected: {
+            backgroundColor: appStyle.selectedDayBackgroundColor,
+            borderRadius: 4,
+        },
+        today: {
+            backgroundColor: appStyle.todayBackgroundColor,
+            borderColor: appStyle.selectedDayBackgroundColor,
+            borderWidth: 1,
+            borderRadius: 4
+        },
+        todayText: {
+            color: appStyle.todayTextColor,
+            fontWeight: 'bold'
+        },
+        selectedText: {
+            color: appStyle.selectedDayTextColor,
+            fontWeight: 'bold'
+        },
+        disabledText: {
+            color: appStyle.textDisabledColor
+        },
+        inactiveText: {
+            color: appStyle.textInactiveColor
+        },
+        dot: {
+            width: 4,
+            height: 4,
+            marginTop: 1,
+            borderRadius: 2,
+            opacity: 0,
+            ...appStyle.dotStyle
+        },
+        visibleDot: {
+            opacity: 1,
+            backgroundColor: appStyle.dotColor
+        },
+        selectedDot: {
+            backgroundColor: appStyle.selectedDotColor
+        },
+        disabledDot: {
+            backgroundColor: appStyle.disabledDotColor || appStyle.dotColor
+        },
+        todayDot: {
+            backgroundColor: appStyle.todayDotColor || appStyle.dotColor
+        },
+        // @ts-expect-error
+        ...(theme['stylesheet.day.basic'] || {})
+    });
+}
